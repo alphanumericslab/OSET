68d59c90ab492de1e14146f7ce4643ac89d37a9e
rsameni <reza.sameni@gmail.com>
2020-09-07 12:45:18 -0400

    Added instantaneous frequency

--- Tools/ChannelIndex0.m
+++ Tools/ChannelIndex0.m
@@ -8,7 +8,7 @@ L1 = size(x,1);
 L2 = size(x,2);
 
 ind = zeros(L1,1); 
-for i = 1:L1,
-    I = find(x(i,:)>=th1(i) & x(i,:)<=th2(i));
+for i = 1:L1
+    I = find(x(i,:) >= th1(i) & x(i,:) <= th2(i));
     ind(i) = 100*length(I)/L2;
 end
