9c5b71b1413934dccd79142b7c38a972576cf108
rsameni <reza.sameni@gmail.com>
2021-05-17 21:45:31 -0400

    Update IntermediatePeakDetection.m

--- Tools/IntermediatePeakDetection.m
+++ Tools/IntermediatePeakDetection.m
@@ -1,6 +1,6 @@
-function interpeaks = IntermediatePeakDetection(x, peaks, wlen, num, varargin)
+function [interpeaks, ref_to_out_lag] = IntermediatePeakDetection(x, peaks, wlen, num, varargin)
 %
-% interpeaks = IntermediatePeakDetection(x, peaks, wlen, num, flag),
+% [interpeaks, ref_to_out_lag] = IntermediatePeakDetection(x, peaks, wlen, num, flag, method),
 % Searches for intermediate peaks between a set of readily found landmark
 % peaks. Useful for detecting P and T wave peaks from the reference R-peaks
 % or for PCG S1 and S2 detection from ECG R-peaks
@@ -12,9 +12,12 @@ function interpeaks = IntermediatePeakDetection(x, peaks, wlen, num, varargin)
 % num: number of intermediate peaks to report
 % flag: search for positive (flag=1) or negative (flag=0) peaks. By default
 % the maximum absolute value of the signal, determines the peak sign.
+% method: model used for calculating the intermediate peaks (see code for
+% functionality). method = 1 by default
 %
-% output:
+% outputs:
 % interpeaks: vector of intermediate peaks impulse train
+% ref_to_out_lag: lag between input ref peaks and output peaks (in method = 2 only)
 %
 % Notes:
 % - The signal baseline wander should be removed before the R-peak detection
@@ -38,6 +41,7 @@ else
     method = 1; % use second method by default
 end
 
+ref_to_out_lag = [];
 if(method == 0) % method 0: find the dominant 'num' local peaks between refs
     N = length(x);
     interpeaks = zeros(1, N);
@@ -77,6 +81,7 @@ elseif(method == 1) % method 1: find the dominant 'num' local peaks between refs
     all_peaks = PeakDetection(x, 1 / wlen, flag);
     
     interpeaks = all_peaks;
+    ref_to_out_lag = zeros(1, length(I_refpeaks));
     for j = 1 : length(I_refpeaks) - 1
         ref_ind_range = I_refpeaks(j) : I_refpeaks(j + 1) - 1;
         x_segment = x(ref_ind_range);
@@ -91,6 +96,7 @@ elseif(method == 1) % method 1: find the dominant 'num' local peaks between refs
         I_absolute_poistion = ref_ind_range(I_segment_peaks(I));
         if(length(I_absolute_poistion) > num)
             interpeaks(I_absolute_poistion(num + 1 : end)) = 0;
+            %             ref_to_out_lag(j) = max(I_absolute_poistion(1 : num)) - I_refpeaks(j);
         end
     end
 elseif(method == 2) % method 2: find the first num local peak after each ref in a window of length wlen
@@ -106,7 +112,8 @@ elseif(method == 2) % method 2: find the first num local peak after each ref in 
     all_peaks = PeakDetection(x, 1 / wlen, flag);
     
     interpeaks = zeros(1, N);
-    for j = 1 : length(I_refpeaks) - 1
+    ref_to_out_lag = zeros(1, length(I_refpeaks));
+    for j = 1 : length(I_refpeaks)
         ref_ind_range = I_refpeaks(j) : min(I_refpeaks(j) + wlen - 1, N);
         x_segment = x(ref_ind_range);
         segment_peaks = all_peaks(ref_ind_range);
@@ -120,6 +127,7 @@ elseif(method == 2) % method 2: find the first num local peak after each ref in 
         I_absolute_poistion = ref_ind_range(I_segment_peaks(I));
         if(~isempty(I_absolute_poistion))
             interpeaks(I_absolute_poistion(1 : num)) = 1;
+            ref_to_out_lag(j) = I_absolute_poistion(1) - I_refpeaks(j);
         end
     end
 else
