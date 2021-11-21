e6fd2cfd54f7c1111eff3a91e96bddd84e7706ef
rsameni <reza.sameni@gmail.com>
2021-11-20 23:00:39 -0500

    Update PeakDetectionProbabilistic.m

--- Tools/PeakDetectionProbabilistic.m
+++ Tools/PeakDetectionProbabilistic.m
@@ -1,8 +1,8 @@
-function [peaks, I_peaks, qrs_likelihood] = PeakDetectionProbabilistic(signal, fs, varargin)
+function [peaks, peak_indexes, qrs_likelihood] = PeakDetectionProbabilistic(signal, fs, varargin)
 % A probabilistic R-peak detector based on local peaks sorting
 %
 % Usage:
-%   [peaks, qrs_likelihood, I_peaks] = PeakDetectionProbabilistic(signal, fs, params)
+%   [peaks, qrs_likelihood, peak_indexes] = PeakDetectionProbabilistic(signal, fs, params)
 %
 % Inputs:
 %   signal: single or multichannel ECG signal with row-wise channels
@@ -14,13 +14,13 @@ function [peaks, I_peaks, qrs_likelihood] = PeakDetectionProbabilistic(signal, f
 %       params.wlen_power_env: power envelope moving average window length (in seconds). Default = 0.03
 %       params.n_bins: number of local peaks histogram bins. Default = max(250, min(500, 10% of signal length))
 %       params.hist_search_th: the top percentile of the signal's power envelope, considered for R-peak detection. Default = 0.9 (top 10%)
-%       params.rpeak_search_wlen: the R-peak detector search window length (in seconds). Default = 0.1
+%       params.rpeak_search_wlen: the R-peak detector search window length (in seconds). Default = 0.2
 %       params.likelihood_sigma: R-peak likelihood estimator STD in seconds (assuming a Gaussian that drops from the peak maximum)
 %       params.max_likelihood_span: R-peak likelihood estimator max window span in seconds (assuming a Gaussian that drops from the peak maximum)
 %
 % Outputs:
 %   peaks: a vector with the signal length with 1's at the estimated R-peaks
-%   I_peaks: the estimated R-peak indexes
+%   peak_indexes: the estimated R-peak indexes
 %   qrs_likelihood: the R-peak likelihood vector (with maximums at the
 %       estimated R-peaks useful for classification and scoring purposes)
 %
@@ -42,7 +42,7 @@ if isfield(params, 'saturate') && isequal(params.saturate, 1)
     if ~isfield(params, 'k_sigma')
         params.k_sigma = 8.0;
     end
-    alpha = k_sigma * std(signal, [], 2);
+    alpha = params.k_sigma * std(signal, [], 2);
     alpha = alpha(:);
     data_sat = diag(alpha) * tanh(diag(1./alpha) * signal);
 else
@@ -84,19 +84,19 @@ bumps_indexes = find(data_bp_env >= bumps_amp_threshold);
 
 % search among the top percentile for the local peaks within a given sliding window length
 if ~isfield(params, 'rpeak_search_wlen')
-    params.rpeak_search_wlen = 0.1;
+    params.rpeak_search_wlen = 0.2;
 end
 rpeak_search_half_wlen = floor(fs * params.rpeak_search_wlen / 2);
-I_peaks = [];
+peak_indexes = [];
 for jj = 1 : length(bumps_indexes)
     index = bumps_indexes(jj);
     segment = max(1, index - rpeak_search_half_wlen) : min(SigLen, index + rpeak_search_half_wlen);
     if max(data_bp_env(segment)) == data_bp_env(index)
-        I_peaks = cat(2, I_peaks, index);
+        peak_indexes = cat(2, peak_indexes, index);
     end
 end
 peaks = zeros(1, SigLen);
-peaks(I_peaks) = 1;
+peaks(peak_indexes) = 1;
 
 % calculate a likelihood function for the R-peaks (useful for classification and scoring purposes)
 if ~isfield(params, 'likelihood_sigma')
