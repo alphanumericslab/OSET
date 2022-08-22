e4b5d35b00e31eec7d84a704d386dc6c5d0f776e
MircDumitru <75252782+MircDumitru@users.noreply.github.com>
2022-08-22 13:02:11 -0400

    For Tikhonov regularization, the .py function is added and the .ipynb test function is added (#3)
    * The 3 functions
    
    * Modifications
    
    * Tikhonov .py function and corresponding .ipynb test function
    
    * Names Modified for Tikhonov functions
    
    * The .py function and tests for Tikhonov filtering

---
+++ Python3/Tools/TikhonovRegularizationMD.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import sparse
+from scipy import linalg
+from scipy.sparse.linalg import lsqr
+def TikhonovRegularizationMD(x, DiffOrderOrFilterCoefs, Lambda):
+    x = np.array(x)
+    if(np.isscalar(DiffOrderOrFilterCoefs) == True):
+        l = np.zeros(DiffOrderOrFilterCoefs, dtype=int).tolist()
+        l.append(1)
+        l.extend(np.zeros(DiffOrderOrFilterCoefs, dtype=int).tolist())
+        h = np.diff(l,DiffOrderOrFilterCoefs).tolist()
+    else:
+        h = DiffOrderOrFilterCoefs
+    L = len(h)
+    N = x.shape[len(x.shape)-1]
+
+    a = np.zeros(N-L, dtype=int).tolist()
+    a.insert(0, h[0])
+    b = h.copy()
+    b.extend(np.zeros(N-L, dtype=int).tolist())
+
+    #from scipy import linalg
+    from scipy import sparse
+    from scipy.sparse.linalg import lsqr
+    #from scipy.sparse.linalg import inv
+
+    D = sparse.csc_matrix(linalg.toeplitz(a,b)) 
+    F = sparse.csc_matrix(Lambda*(D.T*D) + np.eye(N))
+
+    y = lsqr(F.T, x.T, btol = 1e-14,atol = 1e-14)[0].T
+    return y
