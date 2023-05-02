a91779062ea2cc9caf8c176d8d7fb23e93d4f9f9
Reza Sameni <38889933+rsameni@users.noreply.github.com>
2023-05-02 16:54:43 -0400

    Updated the MD5 checksum generator
    Updated the MD5 checksum generator to log relative paths only and surround file name with double quotations

--- Tools/Generic/MD5ChecksumListing.sh
+++ Tools/Generic/MD5ChecksumListing.sh
@@ -1,11 +1,11 @@
-# This script recursively searches a root folder for files with a given extension and logs the MD5 sum of the files and their full path in a CSV file
 #!/bin/bash
+# This bash script recursively searches a root folder for files with a given extension and logs the MD5 sum of the files and their relative path in a CSV file
 
-root_folder="/labs/samenilab/data/physionet.org/files/challenge-2017/1.0.0/training/A01/"
-output_file="./log_file.csv"
+#root_folder="/labs/samenilab/data/physionet.org/files/challenge-2017/1.0.0/training/A01/"
+#output_file="./RECORDS.csv"
 
-# root_folder="/Users/rsameni/Documents/DataFiles/physionet.org/files/challenge-2017/1.0.0/training/A00"
-# output_file="./log_file.csv"
+root_folder="/Users/rsameni/Documents/DataFiles/physionet.org/files/challenge-2017/1.0.0/training/"
+output_file="./RECORDS.csv"
 
 #file_extension=".hea" # file extensions of interest
 file_extension=".mat" # file extensions of interest
@@ -13,17 +13,11 @@ file_extension=".mat" # file extensions of interest
 os=$(uname -s)
 if [[ "$os" == "Linux" ]]; then # For Linux:
     # Recursively search for all files with the specified extension in root_folder and calculate their MD5 sums
-    find "$root_folder" -type f -name "*$file_extension" -exec md5sum {} \; | awk '{gsub(/ /,",",$0); sub(/,/,"",$0); print}' > "$output_file"
+    find "$root_folder" -type f -name "*$file_extension" -exec md5sum {} \; | awk -v root="$root_folder" -v ext="$file_extension" '{gsub(root,"",$2); gsub(ext,"",$2); print $1 "," "\"" $2 "\"" }' > "$output_file"
 
-elif [[ "$os" == "Darwin" ]]; then # For MacOS (avoids missing commas)
-    #find "$root_folder" -type f -name "*$file_extension" -exec md5 {} \; | awk '{gsub(/ /,",",$0); sub(/,/,"",$0); print}' > "$output_file"
-
+elif [[ "$os" == "Darwin" ]]; then # For MacOS:
     # Recursively search for all files with the specified extension in root_folder and calculate their MD5 sums
-    find "$root_folder" -type f -name "*$file_extension" -exec md5 -r {} \; > "$output_file"
-
-    # Replace spaces with commas in the output file to create a CSV file
-    sed -i '' 's/ /,/g' "$output_file"
-
+    find "$root_folder" -type f -name "*$file_extension" -exec md5 -r {} \; | awk -v root="$root_folder" -v ext="$file_extension" '{gsub(root,"",$2); gsub(ext,"",$2); print $1 "," "\"" $2 "\""}' > "$output_file"
 else
     echo "This code only runs on Linux or macOS."
 fi
