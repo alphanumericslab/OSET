f9e534b0e970f4ac91104d9ac4f54ba48a23854a
amulya <amuly.jain@gmail.com>
2023-07-06 13:50:58 -0400

    Updated peak_detection_local_search.py to reflect peak_detection_local_search.m

--- under-development/Amulya_Jain/peak_detection_local_search.py
+++ under-development/Amulya_Jain/peak_detection_local_search.py
@@ -1,10 +1,11 @@
 import numpy as np
+from peak_detection_simple import peak_detection_simple
 
 
-def peak_detection(x, ff, *args):
+def peak_detection_local_search(x, ff, *args):
     """
+    peak_detection_local_search - R-peak detector based on local max/min search
     peaks, peak_indexes = PeakDetection(x,f,flag, num_rounds),
-    R-peak detector based on max search
 
     inputs:
     x: vector of input data
@@ -26,60 +27,36 @@ def peak_detection(x, ff, *args):
     R-peak detection
     """
 
-    def peak_detection_internal(x, ff, flag):
-        n = len(x)
-        peaks = np.zeros(n)
-
-        th = 0.5
-        rng = int(np.floor(th / ff))
-
-        if flag:
-            for j in range(n):
-                if rng < j < n - rng:
-                    index = slice(j - rng - 1, j + rng + 1)
-                elif j > rng:
-                    index = slice((n - 2 * rng) - 1, n + 1)
-                else:
-                    index = slice(0, 2 * rng + 1)
-
-                if np.max(x[index]) == x[j]:
-                    peaks[j] = 1
-        else:
-            for j in range(n):
-                if rng < j < n - rng:
-                    index = slice(j - rng - 1, j + rng + 1)
-                elif j > rng:
-                    index = slice(n - 2 * rng - 1, n + 1)
-                else:
-                    index = slice(0, 2 * rng + 1)
-
-                if np.min(x[index]) == x[j]:
-                    peaks[j] = 1
-
-        # remove fake peaks
-        I = np.where(peaks)[0]
-        d = np.diff(I)
-        peaks[(I[np.where(d < rng)[0]])] = 0
-        peak_indexes = np.where(peaks)[0] + 1
-
-        return peaks, peak_indexes
-
-    # _____________________________________________________________________________________#
-    if len(args) > 2 and args[0] is not None:
+    # Set default values for optional arguments
+    if len(args) > 0 and args[0] is not None:
         flag = args[0]
     else:
         flag = (np.abs(np.max(x))) > np.abs(np.min(x))
 
-    if len(args) > 3 and args[1] is not None:
+    if len(args) > 1 and args[1] is not None:
         num_rounds = args[1]
         if num_rounds > 3:
             raise ValueError('Number of R-peak detections not supported')
     else:
         num_rounds = 1
-    peaks, peak_indexes = peak_detection_internal(x, ff, flag)
+
+    if len(args) > 2 and args[2] is not None:
+        hr_update_fraction = args[2]
+    else:
+        hr_update_fraction = 1.05
+
+    if len(args) > 3 and args[3] is not None:
+        omit_close_peaks = args[3]
+    else:
+        omit_close_peaks = 0
+
+    # Perform peak detection
+    peaks, peak_indexes = peak_detection_simple(x, ff, flag)
+
+    # Perform additional iterations if specified
     if num_rounds > 1:
         for k in range(1, num_rounds):
             rr_intervals = np.diff(peak_indexes)
-            ff = 1.05 / np.median(rr_intervals)  # refined heart rate (in Hz) used for R-peak detection
-            peaks, peak_indexes = peak_detection_internal(x, ff, flag)
+            ff = hr_update_fraction / np.median(rr_intervals)  # refined heart rate (in Hz) used for R-peak detection
+            peaks, peak_indexes = peak_detection_simple(x, ff, flag, omit_close_peaks)
     return peaks, peak_indexes
