1c0556731db19ef61870008fd330a7a8db0ac970
amulya <amuly.jain@gmail.com>
2023-07-06 17:27:54 -0400

    Created the unit test for peak_detection_amp_threshold

---
+++ under-development/Amulya_Jain/Unit-Test/peak_detection_amp_threshold_unit_test.py
@@ -0,0 +1,48 @@
+# For this you need matlab and the new requirements.txt
+import matlab.engine
+import matlab
+import scipy.io
+from peak_detection_amp_threshold import peak_detection_amp_threshold
+
+mat = scipy.io.loadmat('SampleECG1.mat')['data'][0]
+f = 1
+fs = 1000
+th = 0.10  # an arbitrary value for testing
+
+
+def main():
+    ml = runMatLab()
+    py = runPython()
+    return compare_outputs(py[0], ml[0][0]) and compare_outputs(py[1], ml[1][0])
+
+
+def runMatLab():
+    eng = matlab.engine.start_matlab()
+    x = matlab.double(mat.tolist())
+    return eng.peak_detection_amp_threshold(x, f / fs, th, nargout=2)
+
+
+def runPython():
+    return peak_detection_amp_threshold(mat, f / fs, th)
+
+
+def compare_outputs(a, b):
+    x = True
+    try:
+        if a == b:
+            return True
+    except:
+        print('Iterating through the entire array')
+    if not len(a) == len(b):
+        raise Exception('lengths of both inputs have to be the same')
+    for i in range(len(a)):
+        if not (a[i] == b[i]):
+            print(i)
+            print(a[i - 2:i + 3])
+            print(b[i - 2:i + 3])
+            x = False
+    return x
+
+
+if __name__ == "__main__":
+    print(main())
