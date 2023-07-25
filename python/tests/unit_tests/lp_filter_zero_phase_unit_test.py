c4394a9c7958d8dff1c672d07ee33225afcbb505
amulya <amuly.jain@gmail.com>
2023-07-25 14:56:46 -0400

    Updated to use the standard unit test

--- python/tests/unit_tests/lp_filter_zero_phase_unit_test.py
+++ python/tests/unit_tests/lp_filter_zero_phase_unit_test.py
@@ -3,8 +3,8 @@ import matlab
 import matlab.engine
 import numpy as np
 import scipy.io
+import argparse
 from oset.generic.lp_filter.lp_filter_zero_phase import lp_filter_zero_phase
-
 import unit_test as testing
 
 mat = scipy.io.loadmat('../../../datasets/sample-data/SampleECG1.mat')['data'][0]
@@ -32,41 +32,9 @@ def runPython():
     return lp_filter_zero_phase(mat, f / fs)
 
 
-def compare_outputs(a, b):
-    x = True
-    try:
-        if a == b:
-            return True
-    except:
-        print('Iterating through the entire array')
-    if not len(a) == len(b):
-        raise Exception('lengths of both inputs have to be the same')
-    for i in range(len(a)):
-        # a[i] = round(a[i], 8)
-        # b[i] = round(b[i], 8)
-        if not (a[i] == b[i]):
-            print(a[i], "Python\n", b[i], "MatLab")
-            x = False
-    return x
-
-
-def compare_outputs1(a, b):
-    x = True
-    try:
-        if a == b:
-            return True
-    except:
-        print('Iterating through the entire array')
-    if not len(a) == len(b):
-        raise Exception('lengths of both inputs have to be the same')
-    for i in range(len(a)):
-        if not (a[i] == b[i][0]):
-            print(i)
-            print(a[i - 2:i + 3], "python")
-            print(b[i - 2:i + 3], "matlab")
-            x = False
-    return x
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="""This is a unit test for lp_filter_zero_phase"""
+    )
+    args = parser.parse_args()
     print(main())
