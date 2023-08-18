d41abb74d6ec97f73da30e1c36851327df9e2bc6
Reza Sameni <38889933+rsameni@users.noreply.github.com>
2023-08-17 23:05:28 -0400

    updated deprecated ECG dipole generator functions

--- deprecated/DipoleGenerator.m
+++ deprecated/DipoleGenerator.m
@@ -1,4 +1,4 @@
 function [DIP, teta] = DipoleGenerator(N, fs, f, alphai, bi, tetai, teta0)
-% DipoleGenerator has been deprecated. Use dipole_gen_state_space instead.
-warning('DipoleGenerator has been deprecated. Use dipole_gen_state_space instead.');
-[DIP, teta] = dipole_gen_state_space(N, fs, f, alphai, bi, tetai, teta0);
+% DipoleGenerator has been deprecated. Use ecg_dipole_gen_state_space instead.
+warning('DipoleGenerator has been deprecated. Use ecg_dipole_gen_state_space instead.');
+[DIP, teta] = ecg_dipole_gen_state_space(N, fs, f, alphai, bi, tetai, teta0);
