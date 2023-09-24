708bd453e1d32b1d2f59b83f6be7f58f538cc603
Reza Sameni <38889933+rsameni@users.noreply.github.com>
2023-09-24 18:18:34 -0400

    Function name update

--- matlab/tools/plot-and-report/ecg_strip_viewer_standard_grid.m
+++ matlab/tools/plot-and-report/ecg_strip_viewer_standard_grid.m
@@ -122,11 +122,12 @@ for ch = 1 : num_channels %size(data, 1)
 %     col = (num_channels - ch + 1) - (row -1) * cols
     
     horizontal_offset = (col-1)*abs(short_ecg_lens_in_seconds + time_gap_between_plots_in_seconds) + all_channels_left_offset + time_gap_between_plots_in_seconds/2;
+    text_horizontal_offset = -0.25;
     vertical_offset = (row_reversed-1) * vertical_gap_in_mV + first_row_vertical_offset;
     
     plot(ax1, t(n1_small:n2_small) + horizontal_offset, data(ch, n1_small:n2_small) + vertical_offset, 'color', 'k', 'linewidth', 0.25); %lbl = cat(2, lbl, {'Raw signal'});
     hold on
-    text(ax1, t(n1_small) + horizontal_offset, data(ch, n1_small) + vertical_offset + major_amp_ticks_in_mV, ch_names{ch}, 'color', 'b', 'fontsize', 12);
+    text(ax1, t(n1_small) + horizontal_offset + text_horizontal_offset, data(ch, n1_small) + vertical_offset + major_amp_ticks_in_mV, ch_names{ch}, 'color', 'b', 'fontsize', 12);
     %     hold off
     % axis tight
     % daspect(gca, [3.0, t2_small-t1_small, 1.0])
