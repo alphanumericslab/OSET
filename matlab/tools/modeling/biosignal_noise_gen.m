923b69ba6b43fe22d9c8ae989513943cc771056a
giuliabaldazzi <143105310+giuliabaldazzi@users.noreply.github.com>
2023-10-27 12:14:17 -0400

    Added noise extension for seed in biosignal_noise_gen.m

--- matlab/tools/modeling/biosignal_noise_gen.m
+++ matlab/tools/modeling/biosignal_noise_gen.m
@@ -77,10 +77,14 @@ switch noisetype
         if fs ~= 360.0
             artifact = resample(artifact, fs, 360);
         end
+        M = length(artifact);
         if nargin == 6
             n0 = max(1, min(varargin{2}, length(artifact)-N+1));
         else
             n0 = 1;
+        end
+        if M-n0 < N
+            artifact = wextend('1','sym',artifact,N-(M-n0),'r');
         end
         artifact = artifact(n0:N+n0-1)';
         noise = sqrt(NoisePower) * (artifact - mean(artifact)) / std(artifact, 1);
@@ -93,10 +97,14 @@ switch noisetype
         if fs ~= 360.0
             artifact = resample(artifact, fs, 360);
         end
+        M = length(artifact);
         if nargin == 6
             n0 = max(1, min(varargin{2}, length(artifact)-N+1));
         else
             n0 = 1;
+        end
+        if M-n0 < N
+            artifact = wextend('1','sym',artifact,N-(M-n0),'r');
         end
         artifact = artifact(n0:N+n0-1)';
         noise = sqrt(NoisePower) * (artifact - mean(artifact)) / std(artifact, 1);
@@ -109,10 +117,14 @@ switch noisetype
         if fs ~= 360.0
             artifact = resample(artifact, fs, 360);
         end
+        M = length(artifact);
         if nargin == 6
             n0 = max(1, min(varargin{2}, length(artifact)-N+1));
         else
             n0 = 1;
+        end
+        if M-n0 < N
+            artifact = wextend('1','sym',artifact,N-(M-n0),'r');
         end
         artifact = artifact(n0:N+n0-1)';
         noise = sqrt(NoisePower) * (artifact - mean(artifact)) / std(artifact, 1);
@@ -129,10 +141,14 @@ switch noisetype
         load('MA.mat', 'MA'); ma = MA(:, 3);    ma = (ma - mean(ma)) / std(ma);
         artifact = (w_bw * bw + w_em * em + w_ma * ma) / (w_bw + w_em + w_ma);
         artifact = resample(artifact, fs, 360);
+        M = length(artifact);
         if nargin == 7
             n0 = max(1, min(varargin{3}, length(artifact)-N+1));
         else
             n0 = 1;
+        end
+        if M-n0 < N
+            artifact = wextend('1','sym',artifact,N-(M-n0),'r');
         end
         artifact = artifact(n0:N+n0-1)';
         noise = sqrt(NoisePower) * (artifact - mean(artifact)) / std(artifact, 1);
