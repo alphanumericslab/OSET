1c29cd7beb3bcc7c0fb1e1f86ff5c5d89b0d1165
Reza Sameni <38889933+rsameni@users.noreply.github.com>
2023-11-19 07:52:03 -0500

    Added amp_seq_to_stm
    Added a function to convert amplitude sequences to state-transition matrix

--- cpp/fractional-resampler/test_fractional_resampler.m
+++ cpp/fractional-resampler/test_fractional_resampler.m
@@ -1,15 +1,23 @@
 clear; close all; clc
 
+order = 3; % Resampling order (2, 3, 4)
+
 fs_input = 11025;
 fs_output = 4000.0;
-T = 0.1;
+T = 0.5;
 N = round(T * fs_input);
 t_input = (0 : N - 1)/fs_input;
 
 % Sample test of the Resampler class using the mexFunction interface
-signal_input = sin(2 * pi * 157.13 * t_input) + sin(2 * pi * 256.3 * t_input);
+
+% Example 1: A mixture of sinusoidal signals
+% signal_input = sin(2 * pi * 157.13 * t_input) + sin(2 * pi * 256.3 * t_input);
+
+% Example 2: A random low-pass filtered signal
+signal_input = lp_filter_zero_phase(randn(1, N), 150.0 / fs_input);
+
 conversion_rate = fs_input/fs_output; % Resample to half the input sample rate (input/output sampling frequency)
-order = 4; % Use fourth-order interpolation
+
 
 % Call the resampler_mex function to perform resampling
 signal_resampled = resampler_mex(signal_input, conversion_rate, order);
@@ -26,7 +34,10 @@ plot(t_input, signal_input, 'b-', 'linewidth', 2);
 hold on
 plot(t_output, signal_resampled, 'r--', 'linewidth', 2);
 plot(t_input_reconstructed, signal_input_reconstructed, 'g:', 'linewidth', 2);
-legend('Input Signal', 'Resampled Signal', 'Reconstructed Signal');
+legend(['Input Signal @', num2str(fs_input), 'Hz'], ['Resampled Signal @', num2str(fs_output), 'Hz'], ['Reconstructed Signal @', num2str(fs_input), 'Hz']);
 grid
+xlabel('time[s]');
+ylabel('Amplitude');
+set(gca, 'fontsize', 16)
 
-input_vs_reconstructed_delay = finddelay(signal_input , signal_input_reconstructed)
+input_vs_reconstructed_delay = finddelay(signal_input_reconstructed, signal_input)
