fff7f32928601c165c0f715bcbfa9c498f56ae2b
Reza Sameni <38889933+rsameni@users.noreply.github.com>
2026-02-21 10:00:22 -0500

    Fix ring buffer overflow corruption and add regression test (#25)
    Ring-buffer bug fix.

--- cpp/ring-buffer/ring_buffer.cpp
+++ cpp/ring-buffer/ring_buffer.cpp
@@ -3,7 +3,7 @@
 
 RingBuffer::RingBuffer(size_t capacity, size_t inRate, size_t outRate, size_t inBlockSize, size_t outBlockSize, size_t outputOverlap)
     : capacity(capacity), inRate(inRate), outRate(outRate), inBlockSize(inBlockSize), 
-        outBlockSize(outBlockSize), outputOverlap(outputOverlap), inIndex(0), outIndex(0) {
+        outBlockSize(outBlockSize), outputOverlap(outputOverlap), inIndex(0), outIndex(0), size(0) {
     buffer.resize(capacity, 0.0f);
 }
 
@@ -16,16 +16,17 @@ bool RingBuffer::write(const std::vector<float>& data) {
         return false;
     }
 
+    if (inBlockSize > (capacity - size)) {
+        std::cerr << "Buffer overflow." << std::endl;
+        return false;
+    }
+
     for (size_t i = 0; i < inBlockSize; ++i) {
         buffer[inIndex] = data[i];
         inIndex = (inIndex + 1) % capacity;
-        
-        if (inIndex == outIndex) {
-            std::cerr << "Buffer overflow." << std::endl;
-            return false;
-        }
     }
 
+    size += inBlockSize;
     return true;
 }
 
@@ -33,10 +34,9 @@ bool RingBuffer::read(std::vector<float>& data) {
     std::lock_guard<std::mutex> lock(mutex);
     data.clear();
 
-    size_t availableSamples = (inIndex + capacity - outIndex) % capacity;
     size_t expectedSamples = outBlockSize + outputOverlap;
 
-    if (availableSamples < expectedSamples) {
+    if (size < expectedSamples) {
         std::cerr << "Not enough data available." << std::endl;
         return false;
     }
@@ -46,9 +46,13 @@ bool RingBuffer::read(std::vector<float>& data) {
         outIndex = (outIndex + 1) % capacity;
     }
 
+    size -= expectedSamples;
+
     // Adjust for output rate
     outIndex = (outIndex + capacity - (outBlockSize * outRate / inRate)) % capacity;
-    
+    size_t rewind = outBlockSize * outRate / inRate;
+    size += rewind;
+
     return true;
 }
 
